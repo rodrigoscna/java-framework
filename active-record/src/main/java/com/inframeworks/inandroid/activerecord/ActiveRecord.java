5e0561155b9a99b603c48c0b76433a33cbd1074b
Rodrigo Scomazzon do Nascimento <rodrigo.sc.na@gmail.com>
2013-12-25 22:11:56 -0200

    Improving framework's publishing configuration.

--- active-record/src/main/java/com/inframeworks/inandroid/activerecord/ActiveRecord.java
+++ active-record/src/main/java/com/inframeworks/inandroid/activerecord/ActiveRecord.java
@@ -1,10 +1,11 @@
 package com.inframeworks.inandroid.activerecord;
 
+import com.inframeworks.inandroid.activemodel.ActiveModel;
 import com.inframeworks.inandroid.activesupport.annotations.Beta;
 
 /**
  * @author rodrigoscna
  */
 @Beta
-public class ActiveRecord {
+public class ActiveRecord extends ActiveModel {
 }
